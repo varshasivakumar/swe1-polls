8b675d76439a0481973bfb0272f8ee3a85352c02
Varsha Sivakumar <vs2165@nyu.edu>
2019-10-15 15:22:06 -0400

    Reformat settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -113,5 +113,5 @@ if "HOME" in os.environ and "/app" in os.environ["HOME"]:
     django_heroku.settings(locals())
 
 PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_ROOT = os.path.join(PROJECT_DIR, "static")
 STATIC_URL = "/static/"
